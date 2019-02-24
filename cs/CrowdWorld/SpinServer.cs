f2487c63eb4f4dead98174e49efd94a779ff85f3
Kaidong Hu <dongforregister@gmail.com>
2019-02-23 22:55:11 -0500

    .

---
+++ cs/CrowdWorld/SpinServer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace CrowdWorld
+{
+    public class SpinServer
+    {
+          public bool ReqReady, RepReady;
+          public byte[] ReqProto, RepProto;
+          public bool Connected;
+
+          private bool _listenerCancelled;
+          private string sockname;
+          private readonly Thread _listenerWorker;
+
+          private void ListenerWork()
+          {
+              AsyncIO.ForceDotNet.Force();
+              var spin = new SpinWait();
+              using (var server = new ResponseSocket())
+              {
+                  server.Bind(sockname);
+
+                  while (!_listenerCancelled)
+                  {
+                      RepReady = false;
+                      if (!server.TryReceiveFrameBytes(out ReqProto)) {
+                          spin.SpinOnce();
+                          continue;
+                      }
+                      ReqReady = true;
+                      while (!RepReady) {
+                        spin.SpinOnce();
+                      }
+                      ReqReady = false;
+                      server.SendFrame(RepProto);
+                  }
+              }
+              NetMQConfig.Cleanup();
+          }
+
+          public SpinServer(string sname)
+          {
+              sockname = sname;
+              _listenerWorker = new Thread(ListenerWork);
+          }
+
+          public void Start()
+          {
+              _listenerCancelled = false;
+              _listenerWorker.Start();
+          }
+
+          public void Stop()
+          {
+              _listenerCancelled = true;
+              _listenerWorker.Join();
+          }
+    }
+}
