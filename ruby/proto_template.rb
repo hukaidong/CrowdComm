7f0501d525bea10b18e1f3f24dfa686f087ef5ee
Kaidong Hu <dongforregister@gmail.com>
2019-03-15 17:20:44 -0400

    .

---
+++ ruby/proto_template.rb
@@ -0,0 +1,28 @@
+require "securerandom"
+require "CrowdComm/main"
+
+QA = CrowdWorld::Proto::QueryArgs
+
+def cube
+
+  cd = CrowdWorld::Proto::Cube_Data.new.to_h
+
+  CrowdWorld::Proto::Cube.new(
+    id: SecureRandom.hex,
+    config: :CREATE, 
+    data: cd.update(cd) {|k, _| Hash.new }
+  )
+
+end
+
+def agent
+
+  ad = CrowdWorld::Proto::Agent_Data.new.to_h
+
+  CrowdWorld::Proto::Agent.new(
+    id: SecureRandom.hex,
+    config: :CREATE, 
+    data: ad.update(ad) {|k, _| Hash.new }
+  )
+
+end
